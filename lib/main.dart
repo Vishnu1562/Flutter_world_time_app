1d24bf4d28fb954b4733d5f0b6d09da905f92d54
vishnu1562 <vishnuv2754@gmail.com>
2025-07-01 22:28:21 +0530

    added stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('World Time'),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Text('India'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: null,
-      child: Text('click'),
-    ),
-  ),
+  home: Home()
 ));
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('World Time',
+          style: TextStyle(
+            fontSize: 26,
+            fontWeight: FontWeight.w600,
+            color: Colors.black,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.green[600],
+      ),
+      body: Center(
+        child: Text('India',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.green[400],
+      ),
+    );
+  }
+}
 
