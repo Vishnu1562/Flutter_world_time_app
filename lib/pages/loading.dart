6c4c9a246cb5bd951a6edfbc5543a1f7f996d7e7
vishnu1562 <vishnuv2754@gmail.com>
2025-07-03 21:53:57 +0530

    added error handling methods

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+
+import '../services/world_time.dart';
+
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -11,27 +12,28 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getTime() async{
-    Response response = await get(Uri.parse('https://api.api-ninjas.com/v1/worldtime?city=london'));
-    Map data = jsonDecode(response.body);
+  String time = 'loading';
 
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
-
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-  }
+void setUpWorldTime() async {
+  WorldTime instance = WorldTime(location: 'Berlin', flag: 'uk.png', url: '/Berlin');
+  await instance.getTime();
+  setState(() {
+    time = instance.time;
+  });
+}
 
   @override
   void initState() {
     super.initState();
-    getTime();
+    setUpWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Loading'),
+      body: Padding(padding: EdgeInsets.all(50),
+        child: Text(time),
+      ),
     );
   }
 }
---
+++ lib/services/world_time.dart
@@ -0,0 +1,34 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime{
+  String location = '';
+  String time = '';
+  String flag = '';
+  String url = '';
+
+  WorldTime({required this.location, required this.flag, required this.url});
+
+  Future<void> getTime() async{
+    try{
+      Response response = await get(Uri.parse('https://api.api-ninjas.com/v1/worldtime?city=$url'));
+      Map data = jsonDecode(response.body);
+
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1,3);
+
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+
+      time = now.toString();
+    }
+    catch(e){
+      print('caught error: $e');
+      time = 'could not get time data';
+    }
+
+
+  }
+}
+
+
